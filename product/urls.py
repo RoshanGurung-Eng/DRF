868f377f5a1ab827aea2fb8971af977d914e98e8
RoshanGurung-Eng <roshantheaomine@gmail.com>
2024-10-21 17:21:39 +0545

    change in url

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("category",ProductCategoryView.as_view(), name="productcategory-list"),
     path("create/category",ProductCreateCategoryView.as_view(), name="productcategory-create"),
     path("product",ProductView.as_view(), name="product-list"),
-    path("create/product",ProductCreateView.as_view(), name="product-create")
+    path("create/product",ProductCreateView.as_view(), name="product-create"),
+    path("product",ProductView.as_view(), name="product-list"),
+    path("product",ProductView.as_view(), name="product-list"),
     
 ]
