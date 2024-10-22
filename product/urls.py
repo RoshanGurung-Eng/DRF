28bd85c95d8ba6dc81c511e7f1e2ad4b7e1dcbd0
RoshanGurung-Eng <roshantheaomine@gmail.com>
2024-10-22 18:39:46 +0545

    Initial Commit

--- product/urls.py
+++ product/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
 from .views import *
-
 urlpatterns = [
-    #for Category
+    path('', home, name='home'),
+    #category CRUD
     path("category",ProductCategoryView.as_view({'get': 'list'}), name="category"),
     path("create/category",ProductCategoryCreateView.as_view(), name="category"),
-    #For Product
+
+    # Product list View
     path("product",ProductView.as_view(), name="product"),
     path("create/product",ProductCreateView.as_view(), name="product"),
     path("getproduct/<int:id>",ProductGetbyidView.as_view(), name="product"),
     path("updateproduct/<int:id>",ProductUpdateView.as_view(), name="product"),
     path("deleteproduct/<int:id>",ProductDeleteView.as_view(), name="product"),
-    #for COntact
-    path("contactus",ContactGetbyidView.as_view(), name="contactus"),
-    path("updatecontact",ConatactUpdateView.as_view(), name="contactus"),
-    path("deletecontact",ContactDeleteView.as_view(), name="contactus")
 
+    # Contact us
+    path("contactus",ContactGetView.as_view(), name="contactus"),
+    path("create/contactus",ContactCreateView.as_view(), name="contactus"),
+    path("delete/contactus/<int:id>",ContactDestroyView.as_view(), name="contactus"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .serializations import *
-from .models import *
+from .models import * 
 from rest_framework.response import Response
+
+
 
 # Create your views here.
 
-#class ProductCategoryView(generics.ListAPIView):
-#    queryset = ProductCategory.objects.all()
+# class ProductCategoryView(generics.ListAPIView):
+#     queryset = ProductCategory.objects.all()
 #    serializer_class = ProductCategorySerialization
+
+
+def home(request):
+    products = Product.objects.all()  # Fetch products for template rendering
+    return render(request, 'template/base.html', {'products': products})
 
 class ProductCategoryView(viewsets.ViewSet):
     def list(self, request):
-        queryset = ProductCategory.objects.all()
-        serializer = ProductCategorySerialization(queryset, many= True)
-        return Response(serializer.data)
-
+       queryset = ProductCategory.objects.all()
+       serializer = ProductCategorySerialization(queryset, many=True)
+       return Response(serializer.data)
 
 class ProductCategoryCreateView(generics.CreateAPIView):
     queryset = ProductCategory.objects.all()
@@ -24,6 +30,7 @@ class ProductCategoryCreateView(generics.CreateAPIView):
 class ProductView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerialization
+    
 
 class ProductCreateView(generics.CreateAPIView):
     queryset = Product.objects.all()
@@ -41,15 +48,16 @@ class ProductDeleteView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerialization
 
-class ContactGetbyidView(generics.ListAPIView):
+#for the Contact US Serializer
+class ContactGetView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerialization
+    # permession = [permissions.IsAuthenticatedOrReadOnly]
+
+class ContactCreateView(generics.CreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerialization
 
-class ConatactUpdateView(generics.UpdateAPIView):
+class ContactDestroyView(generics.DestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerialization
-
-class ContactDeleteView(generics.DestroyAPIView):
-    queryset = Contact.objects.all()
-    serializer_class = ContactSerialization
-
